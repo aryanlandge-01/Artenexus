a082b060eba9105514ee8bd20f491592ec1ac76c
aryanlandge-01 <landgearyan0@gmail.com>
2024-04-11 14:55:02 +0530

    Initialization

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <h1 className="my-20 mx-28 text-3xl font-serif text-slate-600 font-medium">Welcome to the Artnexus.</h1>
+  )
+}
+
+export default App
